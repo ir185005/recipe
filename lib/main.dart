038e4c68dd119d31f7e83fcf9e38d13213b168b4
Ildar Rakhmatullin <ir185005@mail.ru>
2022-04-06 16:04:02 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/recipe.dart';
+import 'package:recipe/recipe_detail.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: ThemeData(appBarTheme: AppBarTheme(color: Colors.grey))
+          .copyWith(backgroundColor: Colors.grey),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe Calculator'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: ListView.builder(
+              itemCount: Recipe.samples.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    }));
+                  },
+                  child: buildRecipeCard(Recipe.samples[index]),
+                );
+              })),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(recipe.imageUrl),
+            ),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
